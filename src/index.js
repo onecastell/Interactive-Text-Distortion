1e1f76466054e5c07a4aa3ffe0f787b30180444b
onecastell <bloofrank55@gmail.com>
2019-08-18 18:07:10 -0400

    chore: use google style guide and code cleanup

--- src/index.js
+++ src/index.js
@@ -1,65 +1,53 @@
-// import { Blotter } from '../libs/blotter.min.js'
-// import { LiquidDistortMaterial } from '../materials/liquidDistortMaterial.min.js'
-
-let textArray = Array.from("Physics").map(
-  char =>
-    new Blotter.Text(String(char), {
-      family: "Berkshire Swash",
-      weight: 180,
-      size: "100",
-      fill: "#ffffFF",
-      paddingLeft: 4,
-      paddingRight: 15
-    })
+const textArray = Array.from('Physics').map(
+    (char) =>
+      new Blotter.Text(String(char), {
+        family: 'Berkshire Swash',
+        weight: 180,
+        size: '100',
+        fill: '#ffffFF',
+        paddingLeft: 4,
+        paddingRight: 15,
+      })
 );
 
 const material = new Blotter.LiquidDistortMaterial();
 material.uniforms.uSpeed.value = 0.5;
 material.uniforms.uVolatility.value = 0.02;
-let touched = "notTouched";
 
-for (let v in textArray) {
-  const div = document.createElement("div");
+for (let v=0, length=textArray.length; v<length; v++) {
+  // Each text character will be rendered as a canvas nested in a div element
+  const div = document.createElement('div');
   div.id = `panel${v}`;
   document.body.appendChild(div);
 
-  const incrVolatility = event => {
+  const incrVolatility = (event) => {
     let timeout = 0;
-    if (touched === "notTouched") {
-      touched = "touched";
-    }
-    console.log(touched);
-    if (touched === "touched") {
-      for (let i = material.uniforms.uVolatility.value; i < 0.2; i += 0.005) {
-        timeout += 50;
-        setTimeout(() => {
-          material.uniforms.uVolatility.value = i;
-          material.uniforms.uSpeed.value -= 0.0005;
-        }, timeout);
-      }
+    for (let i = material.uniforms.uVolatility.value; i < 0.2; i += 0.005) {
+      timeout += 50;
+      setTimeout(() => {
+        material.uniforms.uVolatility.value = i;
+        material.uniforms.uSpeed.value -= 0.0005;
+      }, timeout);
     }
   };
 
-  const decrVolatility = event => {
+  const decrVolatility = (event) => {
     let timeout = 0;
-
-    if (touched === "touched") {
-      for (let i = material.uniforms.uVolatility.value; i > 0.02; i -= 0.005) {
-        timeout += 50;
-        setTimeout(() => {
-          material.uniforms.uVolatility.value = i;
-        }, timeout);
-      }
+    for (let i = material.uniforms.uVolatility.value; i > 0.02; i -= 0.005) {
+      timeout += 50;
+      setTimeout(() => {
+        material.uniforms.uVolatility.value = i;
+      }, timeout);
     }
   };
 
   // Input Listeners
-  div.addEventListener("mouseenter", incrVolatility);
-  div.addEventListener("mouseleave", decrVolatility);
-  div.addEventListener("touchstart", incrVolatility);
-  div.addEventListener("touchend", decrVolatility);
+  div.addEventListener('mouseenter', incrVolatility);
+  div.addEventListener('mouseleave', decrVolatility);
+  div.addEventListener('touchstart', incrVolatility);
+  div.addEventListener('touchend', decrVolatility);
 
-  const blotter = new Blotter(material, { texts: textArray[v] });
+  const blotter = new Blotter(material, {texts: textArray[v]});
   const scope = blotter.forText(textArray[v]);
   scope.appendTo(div);
 }
